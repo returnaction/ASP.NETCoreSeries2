50ad2cd5ff80d5720d9caadf4d01c0fb48b62437
returnaction <obergannv@gmail.com>
2023-10-25 12:08:42 -0400

    Add CORE SERVICE CONFIGURATION

--- AccountOwnerServer/AccountOwnerServer/Program.cs
+++ AccountOwnerServer/AccountOwnerServer/Program.cs
@@ -1,3 +1,6 @@
+using AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 namespace AccountOwnerServer
 {
     public class Program
@@ -8,13 +11,28 @@ namespace AccountOwnerServer
 
             // Add services to the container.
 
+            builder.Services.ConfigureCors();
+            builder.Services.ConfigureIISIntegration();
+
             builder.Services.AddControllers();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
+            if (app.Environment.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
             app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
